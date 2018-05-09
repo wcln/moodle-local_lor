3b9a121eda47cedc73f8170a7d65bfd93326dc0d
Colin Bernard <colinjbernard@hotmail.com>
2018-05-08 18:40:40 -0700

    wrote script to add contributors to existing lor items

--- script.php
+++ script.php
@@ -4,45 +4,46 @@ require_once(__DIR__ . '/../../config.php'); // standard config file
 
 
 $row = 1;
-if (($handle = fopen("keywords.csv", "r")) !== FALSE) {
+if (($handle = fopen("contributors.csv", "r")) !== FALSE) {
   while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
     $num = count($data);
     echo "<p> $num fields in line $row: <br /></p>\n";
     $row++;
-    $keywords = [];
+    $contributors = [];
     for ($c=1; $c < $num; $c++) {
       if ($data[$c] !== "") {
-        $keywords[] = $data[$c];
+        $contributors[] = $data[$c];
       }
     }
 
     echo $data[0] . "   --  ";
-    var_dump($keywords);
-    addKeywords($data[0], $keywords);
+    var_dump($contributors);
+    addContributors($data[0], $contributors);
   }
   fclose($handle);
 }
 
 
-function addKeywords($content_id, $keywords) {
+function addContributors($content_id, $contributors) {
   global $DB;
 
   echo "inserting into db...";
 
-  // insert into lor_keyword table and lor_content_keywords table
-  foreach ($keywords as $word) {
+  foreach ($contributors as $contributor) {
 
-    echo "\nsearching for keyword '$word'";
-    // check if keyword exists already, if not then insert
-    $existing_record = $DB->get_record_sql('SELECT name FROM {lor_keyword} WHERE name = ?', array($word));
+    echo "\nsearching for contributor '$contributor'";
+    // check if contributor exists already, if not then insert
+    $existing_record = $DB->get_record_sql('SELECT id FROM {lor_contributor} WHERE name = ?', array($contributor));
     echo "here";
     if ($existing_record) {
-      echo "keyword already exists\n";
-      $DB->execute('INSERT INTO {lor_content_keywords}(content, keyword) VALUES (?,?)', array($content_id, $word));
+      echo "contributor already exists\n";
+      $DB->execute('INSERT INTO {lor_content_contributors}(content, contributor) VALUES (?,?)', array($content_id, $existing_record->id));
     } else {
-      echo "inserting new keyword\n";
-      $DB->execute('INSERT INTO {lor_keyword}(name) VALUES (?)', array($word));
-      $DB->execute('INSERT INTO {lor_content_keywords}(content, keyword) VALUES (?,?)', array($content_id, $word));
+      echo "inserting new contributor\n";
+      $record = new stdClass();
+      $record->name = $contributor;
+      $id = $DB->insert_record('lor_contributor', $record);
+      $DB->execute('INSERT INTO {lor_content_contributors}(content, contributor) VALUES (?,?)', array($content_id, $id));
     }
 
   }

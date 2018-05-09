8f585d401867845949944cf792c83c3ac8b0d788
Colin Bernard <colinjbernard@hotmail.com>
2018-05-08 19:52:21 -0700

    added grades to pre existing lor items using script

--- script.php
+++ script.php
@@ -2,48 +2,37 @@
 error_reporting(-1);
 require_once(__DIR__ . '/../../config.php'); // standard config file
 
+$grades_arr = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 $row = 1;
-if (($handle = fopen("contributors.csv", "r")) !== FALSE) {
+if (($handle = fopen("grades.csv", "r")) !== FALSE) {
   while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
     $num = count($data);
     echo "<p> $num fields in line $row: <br /></p>\n";
     $row++;
-    $contributors = [];
-    for ($c=1; $c < $num; $c++) {
-      if ($data[$c] !== "") {
-        $contributors[] = $data[$c];
-      }
+    $grades = [];
+    for ($c=2; $c < $num; $c++) {
+      $grades[] = $data[$c];
     }
 
     echo $data[0] . "   --  ";
-    var_dump($contributors);
-    addContributors($data[0], $contributors);
+    var_dump($grades);
+    addGrades($data[0], $grades);
   }
   fclose($handle);
 }
 
 
-function addContributors($content_id, $contributors) {
+function addGrades($content_id, $grades) {
   global $DB;
+  global $grades_arr;
 
   echo "inserting into db...";
 
-  foreach ($contributors as $contributor) {
+  for ($i = 0; $i < sizeof($grades); $i++) {
 
-    echo "\nsearching for contributor '$contributor'";
-    // check if contributor exists already, if not then insert
-    $existing_record = $DB->get_record_sql('SELECT id FROM {lor_contributor} WHERE name = ?', array($contributor));
-    echo "here";
-    if ($existing_record) {
-      echo "contributor already exists\n";
-      $DB->execute('INSERT INTO {lor_content_contributors}(content, contributor) VALUES (?,?)', array($content_id, $existing_record->id));
-    } else {
-      echo "inserting new contributor\n";
-      $record = new stdClass();
-      $record->name = $contributor;
-      $id = $DB->insert_record('lor_contributor', $record);
-      $DB->execute('INSERT INTO {lor_content_contributors}(content, contributor) VALUES (?,?)', array($content_id, $id));
+    if ($grades[$i] !== "") {
+      $DB->execute('INSERT INTO {lor_content_grades}(content, grade) VALUES (?,?)', array($content_id, $grades_arr[$i]));
     }
 
   }

0384f6c733d466108be7bf7d5225c1d680386bc6
colinbernard <colinjbernard@hotmail.com>
2018-12-30 17:05:11 -0800

    Version -> 3.0

--- version.php
+++ version.php
@@ -3,8 +3,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018122901; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018123000; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016112900; // Requires this Moodle version.
 $plugin->component = 'local_lor'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v2.8';
+$plugin->release = 'v3.0';

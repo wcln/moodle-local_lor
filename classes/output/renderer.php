0e22f6396b15f8cae13864479a70b80aa9665292
colinbernard <colinjbernard@hotmail.com>
2019-01-21 14:24:45 -0800

    Created update_success template

--- classes/output/renderer.php
+++ classes/output/renderer.php
@@ -7,16 +7,15 @@ defined('MOODLE_INTERNAL') || die;
 use plugin_renderer_base;
 
 class renderer extends plugin_renderer_base {
-    /**
-     * Defer to template.
-     *
-     * @param insert_success $page
-     *
-     * @return string html for the page
-     */
+
     public function render_insert_success($page) {
         $data = $page->export_for_template($this);
         return parent::render_from_template('local_lor/insert_success', $data);
+    }
+
+    public function render_insert_update($page) {
+        $data = $page->export_for_template($this);
+        return parent::render_from_template('local_lor/update_success', $data);
     }
 
     public function render_content($page) {

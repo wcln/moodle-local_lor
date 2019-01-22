8753d36d267d59bd654a43779123e1aadc485f00
colinbernard <colinjbernard@hotmail.com>
2019-01-22 12:23:30 -0800

    Version -> 3.7

--- version.php
+++ version.php
@@ -7,4 +7,4 @@ $plugin->version   = 2019012201; // The current plugin version (Date: YYYYMMDDXX
 $plugin->requires  = 2016112900; // Requires this Moodle version.
 $plugin->component = 'local_lor'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v3.6';
+$plugin->release = 'v3.7';

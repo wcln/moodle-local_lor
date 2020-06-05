4c67bc5e8b843e37682822c31ce1f9aa9c6bca6d
Colin Perepelken <colin@perepelken.ca>
2020-06-05 15:17:48 -0700

    Add VueJS integration

---
+++ vue/components/resources.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="resources-index">
+        <h1>{{strings.search_title}}</h1>
+        <p>This content is coming from a Vue component</p>
+    </div>
+</template>
+
+<script>
+    import { mapState } from 'vuex';
+
+    export default {
+        name: "resources-index",
+        computed: mapState(['strings', 'resources'])
+    }
+</script>
+
+<style scoped lang="scss">
+    .resources-index {
+        p {
+            color: red;
+        }
+    }
+</style>
---
+++ vue/main.js
@@ -0,0 +1,49 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import { store } from './store';
+import resources from './components/resources'
+
+function init(contextid) {
+    // We need to overwrite the variable for lazy loading.
+    __webpack_public_path__ = M.cfg.wwwroot + '/local/lor/amd/build/';
+
+    Vue.use(VueRouter);
+
+    store.commit('setContextID', contextid);
+    store.dispatch('loadComponentStrings');
+
+    // You have to use child routes if you use the same component. Otherwise the component's beforeRouteUpdate
+    // will not be called.
+    const routes = [
+        { path: '/', redirect: { name: 'resources-index' }},
+        { path: '/resources', component: resources, name: 'resources-index', meta: { title: 'search_title' }},
+    ];
+
+    // base URL is /local/lor/index.php/
+    const currenturl = window.location.pathname;
+    const base = currenturl.substr(0, currenturl.indexOf('.php')) + '.php/';
+
+    const router = new VueRouter({
+        mode: 'history',
+        routes,
+        base
+    });
+
+    router.beforeEach((to, from, next) => {
+        // Find a translation for the title.
+        if (to.hasOwnProperty('meta') && to.meta.hasOwnProperty('title')) {
+            if (store.state.strings.hasOwnProperty(to.meta.title)) {
+                document.title = store.state.strings[to.meta.title];
+            }
+        }
+        next()
+    });
+
+    new Vue({
+        el: '#local-lor-app',
+        store,
+        router,
+    });
+}
+
+export { init };

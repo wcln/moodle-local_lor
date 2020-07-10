9e75d12a9f45dcf73bd0bdb31efaa56689323001
Colin Perepelken <colin@perepelken.ca>
2020-07-10 13:53:14 -0700

    Revert "Fix saving files to repository from filemanager form submission"
    This reverts commit 8afd8807152acf26664260687e080fa7f5a31039.

--- classes/repository.php
+++ classes/repository.php
@@ -78,7 +78,7 @@ class repository
      * @param  array  $elements
      *                  Should be in format: [ 'name' => 'the form name', 'filepath' => 'The path within the repo to store' ]
      *
-     * @return array Results in format [ 'element_name' => 'formatted filename' ]
+     * @return array Results in format [ 'element_name' => 'saved_filepath' ]
      * @throws coding_exception
      * @throws dml_exception
      */
@@ -98,7 +98,7 @@ class repository
             $files = $fs->get_area_files($context->id, 'local_lor', 'temp', $draftitemid);
 
             foreach ($files as $file) {
-                $filepath = self::get_path_to_repository().self::format_filepath($element['filepath']);
+                $filepath = self::get_path_to_repository().self::format_filename($element['filepath']);
 
                 $file->copy_content_to($filepath);
                 $file->delete();
@@ -111,18 +111,17 @@ class repository
     }
 
     /**
-     * Clean and format a filename within a filepath
+     * Clean and format a filename
      *
-     * @param $filepath string A filepath, for example 'projects/MyProject.pdf'
+     * @param $filename string A filename, for example 'MyProject.pdf'
      *
-     * @return string The same filepath, with the filename formatted
+     * @return string
      */
-    public static function format_filepath($filepath)
+    public static function format_filename($filename)
     {
         // Separate filename into the basename and the file extension (.pdf, .jpg etc...)
-        $file_extension = pathinfo($filepath, PATHINFO_EXTENSION);
-        $filename       = pathinfo($filepath, PATHINFO_FILENAME);
-        $dirname        = pathinfo($filepath, PATHINFO_DIRNAME);
+        $file_extension = pathinfo($filename, PATHINFO_EXTENSION);
+        $filename       = pathinfo($filename, PATHINFO_FILENAME);
 
         // Remove anything which isn't a word, whitespace, number
         // Adapted from: https://stackoverflow.com/a/2021729
@@ -134,7 +133,6 @@ class repository
         // Convert to lowercase
         $filename = strtolower($filename);
 
-        // Rebuild the filepath using the formatted filename
-        return "$dirname/$filename.$file_extension";
+        return "$filename.$file_extension";
     }
 }
--- file.php
+++ file.php
@@ -3,7 +3,7 @@
 require_once('../../config.php');
 require_once('../../lib/filelib.php');
 
-$path = required_param('path', PARAM_TEXT);
+$path = required_param('path', PARAM_URL);
 $filename = required_param('filename', PARAM_TEXT);
 
 send_file(\local_lor\repository::get_path_to_repository() . $path, $filename);

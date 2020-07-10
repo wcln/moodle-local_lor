0daebae209d5f074a40141991bc01a63033198f3
Colin Perepelken <colin@perepelken.ca>
2020-07-10 15:50:02 -0700

    Allow editing projects without overwriting existing files

--- classes/type/project/lang/en/lortype_project.php
+++ classes/type/project/lang/en/lortype_project.php
@@ -10,3 +10,11 @@ $string['document']          = 'Word document';
 $string['document_help']
                              = 'Upload a Word document version of the project. This is required in case any changes to the project are needed. This will overwrite any existing file.';
 $string['download_document'] = 'Download Word document';
+
+$string['edit_existing_files_info'] = '<p>Only upload new files if you wish to overwrite the existing ones. The existing files are:</p>
+<ul>
+    <li><a target="_blank" href="{$a->pdf_link}">PDF</a></li>
+    <li><a target="_blank" href="{$a->document_link}">Word document</a></li>
+</ul>
+<br>
+';

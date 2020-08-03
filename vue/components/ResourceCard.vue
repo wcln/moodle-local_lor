ccb5653305d49f701f9799f7461b47d8b224dbc5
Colin Perepelken <colin@perepelken.ca>
2020-08-03 14:36:42 -0700

    Make all cards on resources search page full height

--- vue/components/ResourceCard.vue
+++ vue/components/ResourceCard.vue
@@ -55,4 +55,14 @@
     .card-image .image img {
         object-fit: cover;
     }
+
+    .card {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+
+      .card-footer {
+        margin-top: auto;
+      }
+    }
 </style>
